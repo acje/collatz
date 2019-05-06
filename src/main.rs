b2315c27f866b178a83926a64fd48b3d476d3cc5
Anders <anders.c.jensen@gmail.com>
2019-05-06 15:05:53 +0200

    add counters

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 fn main() {
     let mut n: u64 = 133713371337;
+    let mut up: u64 = 0;
+    let mut down: u64 = 0;
     while n != 1 {
         match n % 2 == 0 {
-            true => n = n / 2,
+            true => {down +=1;
+            n = n / 2},
             false => {
-                println!("Up!");
+                up += 1; 
                 n = (3 * n) + 1
             }
         }
         println!("{}", n);
     }
+    println!("Steps: {}, Up: {}, Down: {}", up+down, up, down);
 }
