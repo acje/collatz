90d5f10182537b3911d3c66580f9a10a6a80eda5
Anders Jensen <anders.c.jensen@gmail.com>
2019-05-06 21:42:19 +0200

    check for max number of (n*3)+1

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
 fn main() {
-    let mut n: u64 = 133713371337;
-    let mut up: u64 = 0;
-    let mut down: u64 = 0;
-    while n != 1 {
-        match n % 2 == 0 {
-            true => {down +=1;
-            n = n / 2},
-            false => {
-                up += 1; 
-                n = (3 * n) + 1
+    let mut max: u32 = 0;
+    for i in 1..64_000_000 {
+        let mut n: u64 = i;
+        let mut up: u32 = 0;
+        while n != 1 {
+            match n & 1 == 0 {
+                true => n = n / 2,
+                false => {
+                    up += 1;
+                    n = (3 * n) + 1
+                }
             }
         }
-        println!("{}", n);
+        if up > max {
+            println!("{}: new max up: {}", i, up);
+            max = up;
+        };
     }
-    println!("Steps: {}, Up: {}, Down: {}", up+down, up, down);
 }
