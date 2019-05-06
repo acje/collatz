86191258dcf7b350b739228a8fefdd5e667fefb1
Anders <anders.c.jensen@gmail.com>
2019-05-06 14:46:08 +0200

    first

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let mut n: u64 = 133713371337;
+    while n != 1 {
+        match n % 2 == 0 {
+            true => n = n / 2,
+            false => {
+                println!("Up!");
+                n = (3 * n) + 1
+            }
+        }
+        println!("{}", n);
+    }
 }
